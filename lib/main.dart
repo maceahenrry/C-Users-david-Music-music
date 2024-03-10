b6640498c2f3b18a16d0b46e66c78bbd71708dc3
maceahenrry <maceahenrry@gmail.com>
2024-03-10 21:27:29 +0100

    feat upedate readme.md

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Music Player',
+        title: 'Mjjjjjj',
         theme: miTema,
         home: MusicPlayerPage()
       ),
